2f3da2bb524e5d7efe482c763956160569f07bee
LiliMuoz <97261691+LiliMuoz@users.noreply.github.com>
2025-09-03 00:08:58 -0500

    Update auth.controller.mjs

--- src/auth/auth.controller.mjs
+++ src/auth/auth.controller.mjs
@@ -1,7 +1,12 @@
 import { authService } from "./auth.service.mjs";
 
 export async function login(req, res) {
-    const {email, password} = req.body
-    const records = await authService.login(email, password)
-    return res.json(records)
+    // Agregar manejo de errores:
+    try {
+      const result = await AuthService.login(req.body);
+      res.status(200).json(result);
+    } catch (error) {
+      console.error("Login error:", error);
+      res.status(401).json({ error: error.message });
+    }
 }
